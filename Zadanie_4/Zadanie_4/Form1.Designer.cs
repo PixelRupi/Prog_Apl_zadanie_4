538f70cd779cd3ce5423dde078303a8091bb32ed
PixelRupi <piotrekb.345@gmail.com>
2023-11-01 11:57:54 +0100

    Add files via upload

---
+++ Zadanie_4/Zadanie_4/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Zadanie_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button_file_open = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button_file_open
+            // 
+            this.button_file_open.Location = new System.Drawing.Point(12, 12);
+            this.button_file_open.Name = "button_file_open";
+            this.button_file_open.Size = new System.Drawing.Size(143, 73);
+            this.button_file_open.TabIndex = 1;
+            this.button_file_open.Text = "Open File";
+            this.button_file_open.UseVisualStyleBackColor = true;
+            this.button_file_open.Click += new System.EventHandler(this.button_file_open_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(161, 12);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Bubble;
+            series2.Legend = "Legend1";
+            series2.Name = "max";
+            series2.YValuesPerPoint = 2;
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(1082, 494);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1255, 518);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button_file_open);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button_file_open;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ Zadanie_4/Zadanie_4/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zadanie_4
+{
+    public partial class Form1 : Form
+    {
+        string[] koords_bad;
+        double[,] koords;
+        string content;
+        double max_wartosc_x = 0, max_wartosc_y = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_file_open_Click(object sender, EventArgs e)
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+
+
+            //caly ten pier.. od plikow
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+
+                    //Read the contents of the file into a stream
+                    var fileStream = openFileDialog.OpenFile();
+
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        string[] test = null;
+                        double koord_x, koord_y;
+
+                        fileContent = reader.ReadToEnd();
+                        koords_bad = fileContent.Split('\n');
+
+                        Console.WriteLine(koords_bad.ToString());
+                        for (int i = 0; i < 499; i++)
+                        {
+                            string temp = koords_bad[i];
+                            //Console.WriteLine(temp);
+                            //Console.WriteLine("type: temp - " + temp.GetType());
+                            test = temp.Split(' ');
+                            //Console.WriteLine("x: " + test[0] + " y: " + test[1]);
+
+
+                            //Console.WriteLine("type: test[0] - " + test[0].GetType());
+                            //Console.WriteLine("type: test[1] - " + test[1].GetType());
+                            string s_koord_x = test[0];
+                            string s_koord_y = test[1];
+                            // test[0] oraz test[1] sa teraz stringami, parse na double
+                            //zly znak oddzielenia, konwersja kropki na przecinek
+
+                            s_koord_x = s_koord_x.Replace('.', ',');
+                            s_koord_y = s_koord_y.Replace('.', ',');
+
+                            chart1.Series["Series1"].Points.AddXY(s_koord_x, s_koord_y);
+
+                            if (double.TryParse(s_koord_x, out koord_x))
+                            {
+                                Console.WriteLine("Koordynat x: " + koord_x);
+                            } else
+                            {
+                                Console.WriteLine("Koordynat x parse failed");
+                            }
+
+                            if (double.TryParse(s_koord_y, out koord_y))
+                            {
+                                Console.WriteLine("Koordynat y: " + koord_y);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Koordnat y parse failed");
+                            }
+
+
+                            if (koord_y >= max_wartosc_y)
+                            {
+                                max_wartosc_y = koord_y;
+                                max_wartosc_x = koord_x;
+                            }
+
+
+                            /**
+                            koords[i, 0] = koord_x;
+                            koords[i, 1] = koord_y;
+                            **/
+
+                        }
+                        //Console.WriteLine(test);
+                        // koniec loopa
+
+                        Console.WriteLine("Maksymalna wartość to: " + max_wartosc_y + " dla koordynatu x: " + max_wartosc_x);
+                        //chart1.Series["max"].Points.AddXY(max_wartosc_x, max_wartosc_y);
+                    }
+                }
+
+                //MessageBox.Show(fileContent, "File Content at path: " + filePath, MessageBoxButtons.OK);
+            }
+
+
+
+            //koniec
+        }
+    }
+}
